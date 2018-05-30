2c8b5ce4dcad52e3bb84bf16f35bd70823c72c9b
pbafff <andre.myers99@gmail.com>
2018-05-30 19:05:07 -0400

    app serving dist and using history middleware

---
+++ app/client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div.home
+    img(src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
+</template>
+
+<script>
+// @ is an alias to /src
+/*eslint-disable */
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
