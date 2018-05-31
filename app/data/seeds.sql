5f8e84b433a833d37fb55f8a43252f0ca9b93883
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-05-31 18:31:02 -0400

    updating seeds file

---
+++ app/data/seeds.sql
@@ -0,0 +1,17 @@
+use marvel_db;
+--X-Men:
+INSERT INTO movieCharacters (movies,characters) VALUES ("X-men","Charles Xavier"), ("X-men", "Logan") ,("X-men", "Jean Grey"),
+ ("X-men","Senator Kelly"), ("X-men", "Charles Xavier") ,("X-men",  "Sabretooth"),("X-men","Toad"), ("X-men", "Mystique") ,("X-men",  "Cyclops"),
+  ("X-men","Rogue"), ("X-men", "Jean Grey") ,("X-men",  "Magneto"),("X-men","Storm"), ("X-men", "Logan") ,("X-men",  "Wolverine"),
+  ("X-men",  "Professor X");
+
+--The Fantastic Four:
+INSERT INTO movieCharacters (movies,characters) VALUES ("The Fantastic Four","Johnny Storm"), ("The Fantastic Four","Ben Grimm"), 
+("The Fantastic Four","Alicia Masters"), ("The Fantastic Four","Victor Von Doom"), ("The Fantastic Four","Thing");
+
+--Captain America:
+
+INSERT INTO movieCharacters (movies,characters) VALUES ("Captain America","Steve Rogers");
+
+--Blade:
+INSERT INTO movieCharacters (movies,characters) VALUES ("Blade","Deacon Frost"), ("Blade","Whistler"), ("Blade","Mercury"), ("Blade","Blade");
