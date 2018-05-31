93ca36e708203697aafc4ab634f898c712ea1a5f
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-05-31 19:18:36 -0400

    updated seeds

--- app/data/seeds.sql
+++ app/data/seeds.sql
@@ -30,3 +30,27 @@ INSERT INTO movieCharacters (movies,characters) VALUES ("Spider-man","Mary Jane 
 --Daredevil:
 INSERT INTO movieCharacters (movies,characters) VALUES ("Daredevil", "Daredevil"),("Daredevil", "Matt Murdock"),("Daredevil", "Elektra Natchios"),("Daredevil","Bullseye"),
 ("Daredevil","The Kingpin"),("Daredevil","Wilson Fisk"),("Daredevil","Franklin Nelson"),("Daredevil", "Foggy");
+
+--Spider-man 2:
+INSERT INTO movieCharacters (movies,characters) VALUES ("Spider-man 2","Peter Parker"),("Spider-man 2" ,"Mary Jane Watson"),("Spider-man 2","Harry Osborn"),
+("Spider-man 2" ,"J. Jonah Jameson"), ("Spider-man 2","John Jameson"), ("Spider-man 2","May Parker"), ("Spider-man 2","Peter Parker"),
+("Spider-man 2", "Norman Osborn"), ("Spider-man 2","Doctor Octopus"), ("Spider-man 2","Spider-Man");
+
+-- Blade: Trinity:
+INSERT INTO movieCharacters (movies,characters) VALUES ("Blade: Trinity","Whistler"), ("Blade: Trinity","Blade"), ("Blade: Trinity","Hannibal King");
+
+--Hulk:
+INSERT INTO movieCharacters (movies,characters) VALUES ("Hulk","Glenn Talbot"), ("Hulk","Betty Ross"), ("Hulk","Bruce Banner"), ("Hulk","Bruce Banner");
+
+--X-Men: The Last Stand:
+INSERT INTO movieCharacters (movies,characters) VALUES ("X-Men: The Last Stand","Logan"), ("X-Men: The Last Stand","Charles Xavier"), ("X-Men: The Last Stand","Jean Grey"),
+("X-Men: The Last Stand", "Logan"), ("X-Men: The Last Stand","Mystique"), ("X-Men: The Last Stand","Shadowcat"), ("X-Men: The Last Stand","Pyro"), ("X-Men: The Last Stand","Iceman")
+,("X-Men: The Last Stand", "Beast"), ("X-Men: The Last Stand","Rogue"), ("X-Men: The Last Stand","Jean Grey"), ("X-Men: The Last Stand","Professor X"), 
+("X-Men: The Last Stand","Charles Xavier"), ("X-Men: The Last Stand","Magneto"), ("X-Men: The Last Stand","Storm"), ("X-Men: The Last Stand","Wolverine");
+
+--Ghost Rider:
+INSERT INTO movieCharacters (movies,characters) VALUES ("Ghost Rider","Johnny Blaze"), ("Ghost Rider","Roxanne Simpson"), ("Ghost Rider","Blackheart"), ("Ghost Rider","Mephistopheles"),
+("Ghost Rider", "Blackheart");
+
+--Elektra:
+INSERT INTO movieCharacters (movies,characters) VALUES ("Elektra","Typhoid Mary"), ("Elektra","Stick"), ("Elektra","Tattoo"), ("Elektra","Typhoid Mary"), ("Elektra","Elektra");
