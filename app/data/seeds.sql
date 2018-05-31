3dc6f4d9c856c5147bc057122a7a57c0406b497a
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-05-31 18:58:12 -0400

    updated seeds

--- app/data/seeds.sql
+++ app/data/seeds.sql
@@ -10,8 +10,23 @@ INSERT INTO movieCharacters (movies,characters) VALUES ("The Fantastic Four","Jo
 ("The Fantastic Four","Alicia Masters"), ("The Fantastic Four","Victor Von Doom"), ("The Fantastic Four","Thing");
 
 --Captain America:
-
 INSERT INTO movieCharacters (movies,characters) VALUES ("Captain America","Steve Rogers");
 
 --Blade:
 INSERT INTO movieCharacters (movies,characters) VALUES ("Blade","Deacon Frost"), ("Blade","Whistler"), ("Blade","Mercury"), ("Blade","Blade");
+
+--The Punisher:
+INSERT INTO movieCharacters (movies,characters) VALUES ("The Punisher","Frank Castle"), ("The Punisher","Howard Saint"),("The Punisher", "Frank Castle");
+
+--Blade II:
+INSERT INTO movieCharacters (movies,characters) VALUES ("Blade II","Whistler"),("Blade II", "Blade");
+
+--Spider-man:
+
+INSERT INTO movieCharacters (movies,characters) VALUES ("Spider-man","Mary Jane Watson"),("Spider-man", "Peter Parker"),("Spider-man", "Harry Osborn"), 
+("Spider-man","Ben Parker"), ("Spider-man","J. Jonah Jameson"), ("Spider-man","Betty Brant"), ("Spider-man","Peter Parker"), ("Spider-man","Spider-Man"),
+("Spider-man", "Norman Osborn"), ("Spider-man","May Parker"), ("Spider-man","Norman Osborn");
+
+--Daredevil:
+INSERT INTO movieCharacters (movies,characters) VALUES ("Daredevil", "Daredevil"),("Daredevil", "Matt Murdock"),("Daredevil", "Elektra Natchios"),("Daredevil","Bullseye"),
+("Daredevil","The Kingpin"),("Daredevil","Wilson Fisk"),("Daredevil","Franklin Nelson"),("Daredevil", "Foggy");
