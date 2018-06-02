ad3cb1fbcfe4aabdb66a54ce020e81ca45c5381f
pbafff <andre.myers99@gmail.com>
2018-06-02 01:23:31 -0400

    slider element layout

--- app/client/src/components/Slider.vue
+++ app/client/src/components/Slider.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <transition-group name='fade' tag='div'>
+    <transition name="fade" mode="out-in">
       <div v-for="number in [currentNumber]" :key='number'>
         <img :src="currentImage"
         @mouseover="stopRotation" @mouseout="startRotation"/>
         <h1>{{ heroes[Math.abs(currentNumber) % heroes.length] }}</h1>
-        <button>SEE THE COMIC</button>
-        <span><a @click="prev" href='#'>&#8656;</a> | | <a @click="next" href='#'>&#8658;</a></span>
       </div>
-    </transition-group>
+    </transition>
+    <button>SEE THE COMIC</button>
+    <span><a @click="prev" href='#'>&#8656;</a> | | <a @click="next" href='#'>&#8658;</a></span>
   </div>
 </template>
 
@@ -55,11 +55,16 @@ export default {
 <style scoped>
 img {
   height: 100%;
+  order: 1;
+}
+h1 {
+  order: 2;
 }
 div {
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
 }
 a {
   color: aliceblue;
@@ -71,16 +76,16 @@ button {
   background-color: #568080;
   border: none;
   padding: 5px;
+  order: 3;
+}
+span {
+  order: 4;
 }
 .fade-enter-active, .fade-leave-active {
   transition: all 0.8s ease;
-  overflow: hidden;
-  visibility: visible;
   opacity: 1;
-  position: absolute;
 }
 .fade-enter, .fade-leave-to {
   opacity: 0;
-  visibility: hidden;
 }
 </style>
