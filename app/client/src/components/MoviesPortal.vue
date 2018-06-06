39e60765ab1e035f9269cc2d4319f583f81c5c1b
Andre Myers <andre.myers99@gmail.com>
2018-06-05 23:32:06 -0400

    youtube icon

--- app/client/src/components/MoviesPortal.vue
+++ app/client/src/components/MoviesPortal.vue
@@ -1,23 +1,18 @@
 <template>
-    <carousel class="movie-container" perPage="4" navigationEnabled="true"
-    navigationNextLabel="<div style='color: aliceblue;
-     background-color: #151517; border: none'>▶</div>"
-    navigationPrevLabel="<div style='color: aliceblue;
-     background-color: #151517; border: none'>◀</div>">
-      <slide class="movies" v-for="movie in json" :key="movie.Title">
+    <div class="movie-container">
+      <div class="movies" v-for="movie in json" :key="movie.Title">
         <img class="posters" style="cursor: pointer" :src="movie.Poster"
          :alt="movie.Title" @dblclick.prevent="getCharacters($event)"/>
-      </slide>
-    </carousel>
+      </div>
+    </div>
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel';
 import eventBus from '@/main';
+import youtubeIcon from '@/assets/yt_icon_rgb.png';
 
 export default {
   name: 'MoviesPortal',
-  components: { Carousel, Slide },
   data() {
     return {
       json: [],
@@ -25,6 +20,28 @@ export default {
   },
   mounted() {
     this.getMovies();
+  },
+  updated() {
+    // eslint-disable-next-line
+    $('.movie-container').slick({
+      autoplay: false,
+      centerMode: true,
+      centerPadding: '1px',
+      variableWidth: false,
+      variableHeight: true,
+      infinite: true,
+      dots: false,
+      slidesToShow: 3,
+      adaptiveHeight: true,
+      responsive: [{
+        breakpoint: 500,
+        settings: {
+          dots: false,
+          arrows: false,
+          infinite: false,
+        },
+      }],
+    });
   },
   methods: {
     getMovies() {
@@ -34,10 +51,29 @@ export default {
     },
     getCharacters(event) {
       eventBus.$emit('movieSelected', event.currentTarget.getAttribute('alt'));
+      if (document.getElementsByClassName('highlighted').length > 0) {
+        document.getElementsByClassName('highlighted').item(0).classList.remove('highlighted');
+      }
+      if (document.getElementsByClassName('play').length > 0) {
+        document.getElementsByClassName('play').item(0).remove();
+      }
+      event.currentTarget.classList.add('highlighted');
+      const element = document.createElement('img');
+      element.setAttribute('src', youtubeIcon);
+      element.style.width = '45px';
+      element.style.marginTop = '10px';
+      element.style.marginLeft = '130px';
+      element.classList.add('play');
+      event.currentTarget.parentElement.appendChild(element);
     },
   },
 };
 </script>
 
-<style >
+<style>
+.highlighted {
+  border-style: solid;
+  border-color: aliceblue;
+  text-align: center;
+}
 </style>
